759646c2ca81cf208bb55f11ff4853efbee44d50
Kosorukov Anatolii <java1cprog@gmail.com>
2026-01-17 08:06:25 +0700

    feat(plugin-sdk): add licensing decision types

---
+++ packages/plugin-sdk/src/licensing.ts
@@ -0,0 +1,33 @@
+export type license_error_code =
+  | "license.missing"
+  | "license.entitlement_missing"
+  | "license.entitlement_invalid"
+  | "license.signature_invalid"
+  | "license.hash_mismatch"
+  | "license.expired"
+  | "license.not_yet_valid"
+  | "license.trust_store_missing"
+  | "license.manifest_invalid"
+  | "license.policy_denied";
+
+export type license_diagnostic = {
+  code: license_error_code;
+  message: string;
+  details?: Record<string, unknown>;
+};
+
+export type license_capabilities = {
+  constraints: boolean;
+  pricing: boolean;
+  render: boolean;
+  export: boolean;
+  solver: boolean;
+  ui: boolean;
+};
+
+export type license_decision = {
+  ok: boolean;
+  allow_load: boolean;
+  allow_capabilities: license_capabilities;
+  diagnostics: license_diagnostic[];
+};
